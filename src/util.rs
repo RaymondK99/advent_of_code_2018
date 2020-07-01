20f8c7c86c95fcb152e81ca256357c10af8a1a11
raymondk <raymond@kth.se>
2020-07-01 17:31:10 +0200

    addd day 2

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 mod day_01;
-/*mod day_02;
-mod day_03;
+mod day_02;
+/*mod day_03;
 mod day_04;
 mod day_05;
 mod day_06;
@@ -33,8 +33,7 @@ pub enum Part {
 pub fn get_solution(day:u8, part:Part, input:String) -> String {
     match day {
         1 => day_01::solve(input, part),
-        /*
-        2 => day_02::solve(input, part),
+        2 => day_02::solve(input, part),/*
         3 => day_03::solve(input, part),
         4 => day_04::solve(input, part),
         5 => day_05::solve(input, part),
---
+++ src/util/day_02.rs
@@ -0,0 +1,94 @@
+use super::Part;
+use std::collections::HashSet;
+
+pub fn solve(input : String, part: Part) -> String {
+
+    let lines:Vec<&str> = input.lines()
+        .collect();
+
+    let result = match part {
+        Part::Part1 => part1(lines),
+        Part::Part2 => part2(lines)
+    };
+
+    format!("{}",result)
+}
+
+fn part1(input:Vec<&str>) -> String {
+    let mut twos = 0;
+    let mut threes = 0;
+    for &line in input.iter() {
+        let mut vec :[i32; 128] = [0;128];
+        for ch in line.chars() {
+            let index = ch as usize;
+            vec[index] += 1;
+        }
+
+        for freq in vec.iter() {
+            if *freq == 2 {
+                twos += 1;
+                break;
+            }
+        }
+
+        for freq in vec.iter() {
+            if *freq == 3 {
+                threes += 1;
+                break;
+            }
+        }
+    }
+
+    format!("{}",twos * threes)
+}
+
+fn part2(mut input:Vec<&str>) -> String {
+    let mut res = String::new();
+
+    for i in 0..input.len()-1 {
+        for j in i+1..input.len() {
+            let word1: Vec<char> = input[i].chars().collect();
+            let word2: Vec<char> = input[j].chars().collect();
+
+            let mut diff_sum = 0;
+            res.clear();
+            for n in 0..word1.len() {
+                if word1[n] != word2[n] {
+                    diff_sum += 1;
+                } else {
+                    res.push(word1[n]);
+                }
+            }
+
+            if diff_sum == 1 {
+                return res
+            }
+        }
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use crate::util::Part::Part1;
+
+
+    #[test]
+    fn test1() {
+        let input = "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab";
+        let res = part1(input.lines().collect());
+        println!("{}", res);
+        assert_eq!("12", res);
+    }
+
+    #[test]
+    fn test2() {
+        let input = "abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz";
+        let res = part2(input.lines().collect());
+        println!("{}", res);
+        assert_eq!("fgij", res);
+    }
+}
